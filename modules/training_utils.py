516974916cfeed0a08ccded7cc3e33dcdf8cdbfa
junhanjeong <junhan135790@gmail.com>
2024-11-24 01:11:38 +0900

    tmp

--- modules/training_utils.py
+++ modules/training_utils.py
@@ -38,5 +38,7 @@ def split_dataset(tokenized_dataset, data_args, seed):
     # tokenized_dataset = tokenized_dataset.filter(lambda x: len(x["input_ids"]) <= data_args.tokenize_max_length)
     train_dataset = tokenized_dataset.select(range(1802))
     test_dataset = tokenized_dataset.select(range(1802, 2003))
+    train_dataset = train_dataset.filter(lambda x: len(x["input_ids"]) <= data_args.tokenize_max_length)
+    test_dataset = test_dataset.filter(lambda x: len(x["input_ids"]) <= data_args.tokenize_max_length)
     return train_dataset, test_dataset
     # return tokenized_dataset.train_test_split(test_size=data_args.train_test_split_ratio, seed=seed)
